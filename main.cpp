6a4a23d6cb2866e71caefd1169a1a18709aa6bb1
Utishnik <yrik.kokoyto.00@bk.ru>
2023-10-18 18:03:55 +0300

    creating write

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 
 template <typename T>
-void print2darr(T a,int h,int w)
+void print2darr(T **a,int h,int w)
 {
 	for(int i=0;i<h;i++)
 	{
@@ -24,35 +24,24 @@ void print2darr(T a,int h,int w)
 	}
 }
 
-const int count_col = 2;
-const int max_len_str = 256;
+const int CNT_COL = 2;
+const int MX_LN_STR_BD = 256;
 string path_bd = "baza_data.txt";
 const int MaxCountToken = 1024;
 
 const int type_cnt = 3;
 const int max_len_type_name = 32;
-char map_type[type_cnt][max_len_str] = {
+char map_type[type_cnt][MX_LN_STR_BD] = {
 	"Int",
 	"String",
 	"Double"
 };
 
 
-void write(char str[count_col]);
+
+
 string* read();
 void rwrite();
-
-void write(char str[count_col][max_len_str])
-{
-	ofstream file;
-	file.open(path_bd, ofstream::app);
-
-	if (!file.is_open()) { printf("error open file!\n"); return; }
-	for (int i = 0; i < count_col; i++) file << str[i] << " ";
-	file << endl;
-	file.close();
-
-}
 
 struct DOUBLE {
 	double a;
@@ -84,7 +73,6 @@ string  *read(int *retsize)
 	int double_iterator;
 	int string_iterator;
 	int int_iterator;
-
 	ifstream rfile;
 	rfile.open(path_bd);
 	if (!rfile.is_open()) { printf("error open read file!!!\n"); return NULL; }
@@ -93,7 +81,6 @@ string  *read(int *retsize)
 	int count_str = 0;
 
 	string* db = (string*)malloc(sizeof(string) * Max_Db_Size);
-
 	{
 		int i = 0;
 		while (!rfile.eof())
@@ -105,11 +92,8 @@ string  *read(int *retsize)
 			count_str++;
 		}
 	}
-
 	*retsize=count_str;
-
 	rfile.close();
-
 	return db;
 }
 
@@ -167,10 +151,7 @@ int *find_token( char* str,  char* token,int len_list_token_number,int *len_ret)
 		}
 	}
 
-	//print2darr(tokens,cnt_probel+1,len_list_token_number);
-
 	int *tkfnd_indx=(int*)malloc(sizeof(*tkfnd_indx)*cnt_tk);
-
 	int itr=0;
 
 	for (int i = 0; i < itrator; i++)
@@ -226,15 +207,19 @@ bool str_tojdesto(char *str1,char *str2)
 			break;
 		}
 	}
-
 	return result;
 }
 
-int** find_value_in_bd(char* value, string* db,int count_col,int count_str,int maxlentk,int cntfndtk,int **arrln)
+void err_ptr(void *ptr)
+{
+	printf("error pointer; adress pointer = \t %p",ptr);
+}
+
+int** find_value_in_bd(char* value, string* db,int count_col,int max_len_str,int maxlentk,int cntfndtk,int **arrln,bool *is_empty)
 {
 	////
 	char **arr_db=(char**)malloc(sizeof(char*)*count_col);
-	for(int i=0;i<count_col;i++) arr_db[i]=(char*)malloc(sizeof(char)*count_str);
+	for(int i=0;i<count_col;i++) arr_db[i]=(char*)malloc(sizeof(char)*max_len_str);
 
 	for(int i=0;i<count_col;i++)
 	{
@@ -245,6 +230,7 @@ int** find_value_in_bd(char* value, string* db,int count_col,int count_str,int m
 	}
 
 	int **result=(int**)malloc(sizeof(int*)*count_col);
+	bool empty=false;
 
 	for(int i=0;i<count_col;i++)
 	{
@@ -252,18 +238,17 @@ int** find_value_in_bd(char* value, string* db,int count_col,int count_str,int m
 		result[i]=find_token(arr_db[i],value,cntfndtk,&ln_rt);
 	
 		(*arrln)[i]=ln_rt;
+
+		if(ln_rt>0) empty=true;
+		if(ln_rt<0) err_ptr(((*arrln)+i));
 	}
 
-	//print2darr(result,count_col,cntfndtk);
-
-
+	*is_empty=empty;
 	for(int i=0;i<count_col;i++) free(arr_db[i]);
 	free(arr_db);
 
 	return result;
 }
-
-
 void char_str_init(char *str,const char *str2,int len) { for(int i=0;i<len;i++) str[i]=str2[i];}
 
 template <typename T>
@@ -273,6 +258,31 @@ void init2darr(T ***arr,int h,int w){
 		(*(arr))[i]=(T*)malloc(sizeof((***(arr)))*w);
 }
 
+
+template <typename T>
+void init3darr(T ****arr,int h,int w,int d)
+{
+	(*(arr))=(T***)malloc(sizeof(**(arr))*h);
+		for(int i=0;i<h;i++)
+		(*(arr))[i]=(T**)malloc(sizeof((***(arr)))*w);
+	for(int i=0;i<h;i++)
+		for(int j=0;j<w;j++)
+			((*arr))[i][j]=(T*)malloc(sizeof((****(arr)))*d);
+}
+
+template <typename T>
+void free3darr(T ****arr,int h,int w)
+{
+	for(int i=0;i<h;i++)
+		for(int j=0;j<w;j++)
+			free(((*arr))[i][j]);
+	for(int i=0;i<h;i++)
+		free((*arr)[i]);
+
+	free((*arr));			
+}
+
+
 void free2darr(char ***arr,int h)
 {
 	for(int i=0;i<h;i++)
@@ -280,18 +290,52 @@ void free2darr(char ***arr,int h)
 	free((*arr));
 }
 
-
-
-template <typename T>
-void *Malloc(T ptr,int size)
+//записует в базу данных строку равная ровно количеству столбцов в ней
+//index_unique_col массив индексов которые указают на данные которые должны быть уникальными чтобы
+//строка записалась
+//пример допустим база данных где первый столбец это имена пользователей(уникальные) и второе это их баланс
+//тогда index_unique_col его длина будет равна 1 и он будет равен 0 тоесть первой строке
+bool write_full_str_in_bd(char str[CNT_COL][MX_LN_STR_BD],string *bd,int cnt_str_in_bd,int *index_unique_col,
+int maxlenstr,int maxlentk,int cntfndtk,int count_col_bd,int **str_find_index) //cntfndtk - максимальное колво индесков токенов которое может найти
 {
-	return malloc(size*(*ptr));
+	//str_find_index - массив индексов строк с найдеными уникальными значениями
+	int count_unique_cols=(sizeof(index_unique_col)/sizeof(index_unique_col[0]));
+	char **value_arr=NULL;
+	init2darr(&value_arr,count_unique_cols,maxlenstr);
+
+	int itrator=0;
+	for(int i=0;i<count_unique_cols;i++)
+	{	
+		int ind=index_unique_col[i];
+		{
+			for(int k=0;k<strlen(str[ind]);k++) value_arr[itrator][k]=str[ind][k];
+			itrator++;
+		}
+	}
+
+	int ***arrlen=0;
+	int ***fndarr;
+	init3darr(&arrlen,count_unique_cols,cnt_str_in_bd,count_col_bd);
+	init3darr(&fndarr,count_unique_cols,cnt_str_in_bd,cntfndtk);
+
+	bool is_empty=true;
+	bool empty_fnd_value=true;
+	for(int i=0;i<count_unique_cols;i++)
+	{	
+		if(is_empty) bool empty_fnd_value=true;//нашлось ли строка с уникальным значением
+		fndarr[i]=find_value_in_bd(value_arr[i],bd,count_col_bd,maxlenstr,maxlentk,cntfndtk,arrlen[i],&empty_fnd_value);
+		if(empty_fnd_value==false) is_empty=false;
+	}
+
+	// сделано получения уникальный столбцов осталось прогнать их
+	// и если где то находится значение то возвращается ноль
+	//а если нет нечего то пишется строка и потом сохраняется в фаил
+	//что бы сделать изменение значения нужно проверить есть ли оно и заменить нужный стобец на найденой строке на желаемое значение
 }
 
 
 
-
-int main()
+int main(int argc,char *argv[])
 {
 	string testdb[3] = {
 		"  gggniger  niger 1  ",
@@ -301,6 +345,7 @@ int main()
 
 	char value[10] = "gggniger";
 
+
 	int *lenarray=(int*)malloc(sizeof(int)*3);
 	int **arrret=find_value_in_bd(value,testdb,3,200,200,10,&lenarray);
 
@@ -308,21 +353,10 @@ int main()
 	{
 		for(int j=0;j<lenarray[i];j++)
 		{
-			printf("%d ",arrret[i][j]);
+			//printf("%d ",arrret[i][j]);
 		}
-		printf("\n");
+		//printf("\n");
 	}
-
-
-   //int **arr=find_value_in_bd(value,testdb,3,3,123,128,&arr2d);
-
-
-
-
-
-
-	//print2darr(arr,3,100);
-
 
 	 char o1[1024] = "121 ilia pizdaliz ebaniy hhhh   gfffg 121 121  pi 344334";
 	 char o2[1024] = "  ";
